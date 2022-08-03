6134819849ccbe6d0331d65c191ea2272811badf
marangacyprian <marangacyprian@gmail.com>
2022-08-03 11:48:55 +0300

    initial commit

---
+++ app/src/main/java/com/example/firebase2/Rate.kt
@@ -0,0 +1,6 @@
+package com.example.firebase2
+
+class Rate (val id: String,val name:String, val rating:Int){
+
+    constructor():this("","",0)
+}
---
+++ app/src/main/java/com/example/firebase2/RateAdapter.kt
@@ -0,0 +1,27 @@
+package com.example.firebase2
+
+import android.content.Context
+import android.provider.Telephony
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+
+
+class RateAdapter(val mCtxt: Context, val layoutResId: Int, val RateList: MutableList<Rate>)
+    : ArrayAdapter<Rate>(mCtxt,layoutResId,RateList)
+
+
+{
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val layoutInflater: LayoutInflater = LayoutInflater.from(mCtxt);
+        val view:View= layoutInflater.inflate(layoutResId,null );
+
+        val txt_View= view.findViewById<TextView>(R.id.txt_View);
+
+        val rate= RateList[position];
+        txt_View.text=rate.name;
+        return view;
+    }
+}
